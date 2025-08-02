9a99e2ded31b39adf715401664397986ec01a9c3
Jimmy <JimmyBowcott@users.noreply.github.com>
2025-08-02 17:04:52 +0100

    add canvas component

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,36 @@
 use wasm_bindgen::prelude::*;
-use web_sys::console;
+use wasm_bindgen::JsCast;
+use web_sys::{console, window, HtmlCanvasElement, CanvasRenderingContext2d};
 
 #[wasm_bindgen(start)]
-pub fn start() {
-    console_log("Loaded from WASM!");
+pub async fn start() -> Result<(), JsValue> {
+    console_error_panic_hook::set_once(); // this logs rust panics to the console
+
+    let window = window().unwrap();
+    let document = window.document().unwrap();
+    let canvas = document
+        .get_element_by_id("main-canvas")
+        .unwrap()
+        .dyn_into::<HtmlCanvasElement>()?;
+
+    let context = canvas
+        .get_context("2d")?
+        .unwrap()
+        .dyn_into::<CanvasRenderingContext2d>()?;
+
+    context.set_fill_style_str("white");
+    context.fill_rect(0.0, 0.0, canvas.width().into(), canvas.height().into());
+
+    context.set_stroke_style_str("black");
+    context.set_fill_style_str("black");
+    context.set_line_width(2.0);
+
+    context.begin_path();
+    context.arc(25.0, 25.0, 25.0, 0.0, std::f64::consts::PI * 2.0).unwrap();
+    context.fill();
+
+    console_log("Canvas rendered.");
+    Ok(())
 }
 
 #[wasm_bindgen]
